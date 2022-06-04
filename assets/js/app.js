9839e1acbe8378726f958e4db9a3c7eacc7df227
duchung45644 <duchung45644@gmail.com>
2022-06-04 19:30:18 +0700

    Preview image

---
+++ assets/js/app.js
@@ -0,0 +1,17 @@
+const inputImg = document.querySelector('#img')
+var img = document.querySelector('img');
+
+inputImg.addEventListener('change', (e) => {
+
+    let file = e.target.files[0]
+
+    if (!file) return
+
+    img.src = URL.createObjectURL(file)
+
+    console.log(img);
+
+    document.querySelector('.preview').appendChild(img)
+})
+
+// document.querySelector('.preview').appendChild(img)
